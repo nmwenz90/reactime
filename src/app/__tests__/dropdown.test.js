b5ea597630f71b00975c4c3c8582ae82dd02cfe1
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-07-09 17:07:36 -0700

    added tests to dropdown.test.js

--- src/app/__tests__/dropdown.test.js
+++ src/app/__tests__/dropdown.test.js
@@ -20,15 +20,22 @@ describe('unit testing for Dropdown.jsx', () => {
   };
   beforeEach(() => {
     wrapper = shallow(<Dropdown {...props} />);
+    console.log('setSpeed: ', props.setSpeed);
   });
 
   describe('Component', () => {
     test('array of objects that have value and label should be options props', () => {
       expect(wrapper.props().options).toEqual(props.speeds);
     });
-    test('selectedOption should be value property', () => {
+    test('selectedSpeed should be value property', () => {
       expect(wrapper.props().value).toEqual(props.selectedSpeed);
+      expect(wrapper.props().value).toHaveProperty('value');
+      expect(wrapper.props().value).toHaveProperty('label');
     });
+    test('SelectedSpeed value should be a number, label a string', () => {
+      expect(typeof wrapper.props().value.value).toBe('number');
+      expect(typeof wrapper.props().value.label).toBe('string');
+    })
   });
 
   describe('setSpeed', () => {
